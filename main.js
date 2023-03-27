9e9bec1f6c74a511d80da1c53332af832fdf99af
kfheskes <kayarneheskes@hotmail.com>
2023-03-27 19:13:26 +0200

    test2

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-// [] Functies
+// [x] Functies declareren
+
+function congratulate() {
+    console.log('Gefeliciteerd!');
+}
+
+function giveKisses() {
+    console.log('Smak');
+}
+
+// [x] Functies aanroepen
+
+congratulate();
+// doe dingen
+congratulate();
+// weer dingen
+congratulate();
+giveKisses();
+
+// [x] Functies met parameters (parameters plaatsen wij tussen de ()   )
+function congratulateColleague(name) {
+    console.log('Gefeliciteerd' + name + '!');
+}
+
+congratulateColleague('Marjan');
+congratulateColleague( 'Mitchell');
+congratulateColleague( 'Bas');
+congratulateColleague('Kay');
+
+// [x] Return values
+// width: 4
+// Length: 6
+function area(width, length){
+    const outcome = width*length;
+
+    return outcome;
+}
+
+const livingRoom = area(4, 6);
+const kitchen = area(12,4);
+const bathroom = area(4, 2);
+
+const houseDescription = 'De woonkamer is ' + livingRoom + ' m2, de keuken is ' + kitchen + ' m2 en de bathroom is ' + bathroom + ' m2.'
+
+console.log(houseDescription);
+
+// wanneer je parameter aanroept zoals in area het geval is noem je het argumenten
+// [] Regular vs Arrow functions
+
+const area1 = function (width, length){
+
+    return width*length;
+}
+
+const area2 = (width, length) => width * length;
+
